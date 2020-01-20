dbd87f2ed5212cb77a4c2efc05865610af5dada3
DeemoHarlos <cces4415@gmail.com>
2020-01-20 14:07:11 +0800

    another commit

--- data/data.js
+++ data/data.js
@@ -11,10 +11,13 @@ var data = {
 function getResult(county,town,obj) {
 	obj.president = {}
 	getPresident(county,town,obj.president)
+	obj.party = {}
+	getParty(county,town,obj.party)
 }
 
 function getInfo() {
 	getPresidentInfo()
+	getPartyInfo()
 }
 
 function getPresident(county,town,obj) {
@@ -38,6 +41,27 @@ function getPresident(county,town,obj) {
 	obj.novote  = obj.total - parseInt($2(vote[0]).text().split(',').join(''))
 }
 
+function getParty(county,town,obj) {
+	obj.votes = []
+	var url = `https://www.cec.gov.tw/pc/zh_TW/L4/n${county}00${town}00000000.html`
+	var res = requestSync('GET', url)
+	const $ = cheerio.load(res.getBody())
+	var votes = $('#divContent .trT>td:nth-child(3)')
+	votes.each((i,vote)=>{
+		obj.votes.push(parseInt($(vote).text().split(',').join('')))
+	})
+
+	var url2 = `https://www.cec.gov.tw/pc/zh_TW/S4/${county}00${town}00000000.html`
+	var res2 = requestSync('GET', url2)
+	const $2 = cheerio.load(res2.getBody())
+	var invalid = $2('#divContent .trT>td:nth-child(5)')
+	obj.invalid = parseInt($2(invalid[0]).text().split(',').join(''))
+	var total   = $2('#divContent .trT>td:nth-child(2)')
+	obj.total   = parseInt($2(total[0]).text().split(',').join(''))
+	var vote    = $2('#divContent .trT>td:nth-child(3)')
+	obj.novote  = obj.total - parseInt($2(vote[0]).text().split(',').join(''))
+}
+
 function getPresidentInfo() {
 	data.president.info = []
 	var url = `https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html`
@@ -47,6 +71,21 @@ function getPresidentInfo() {
 	var indexes = $('#divContent .trT>td:nth-child(2)')
 	names.each((i,name)=>{
 		data.president.info.push({
+			'index': parseInt($(indexes[i]).text()),
+			'name' : $(name).text()
+		})
+	})
+}
+
+function getPartyInfo() {
+	data.party.info = []
+	var url = `https://www.cec.gov.tw/pc/zh_TW/L4/n00000000000000000.html`
+	var res = requestSync('GET', url)
+	const $ = cheerio.load(res.getBody())
+	var names   = $('#divContent .trT>td:nth-child(2)')
+	var indexes = $('#divContent .trT>td:nth-child(1)')
+	names.each((i,name)=>{
+		data.party.info.push({
 			'index': parseInt($(indexes[i]).text()),
 			'name' : $(name).text()
 		})
